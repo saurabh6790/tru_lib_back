6579deac057357b61728a42fd71c29a9b41222d3
indictrans <tejal.s@indictranstech.com>
2015-01-29 12:01:55 +0530

    [init file changes]

---
+++ __init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+    werkzeug
+    ~~~~~~~~
+
+    Werkzeug is the Swiss Army knife of Python web development.
+
+    It provides useful classes and functions for any WSGI application to make
+    the life of a python web developer much easier.  All of the provided
+    classes are independent from each other so you can mix it with any other
+    library.
+
+
+    :copyright: (c) 2011 by the Werkzeug Team, see AUTHORS for more details.
+    :license: BSD, see LICENSE for more details.
+"""
+from types import ModuleType
+import sys
+
+
+# the version.  Usually set automatically by a script.
+__version__ = '0.8.3'
